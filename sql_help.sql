0065504c667d3034ba2245c6b79bbb9a1e9b2157
dhopz <david_houpapa@hotmail.com>
2022-04-06 17:19:18 +0100

    sliding windows

--- sql_help.sql
+++ sql_help.sql
@@ -225,3 +225,21 @@ SELECT
 	AVG(m.home_goal + m.away_goal) OVER() AS overall_avg
 FROM match AS m
 LEFT JOIN country AS c ON m.country_id = c.id;
+
+--Sliding Windows
+-- Assessing the running total of home goals scored by FC Utrecht.
+-- Assessing the running average of home goals scored.
+-- Ordering both the running average and running total by date.
+SELECT 
+	date,
+	home_goal,
+	away_goal,
+    -- Create a running total and running average of home goals
+    SUM(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_total,
+    AVG(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_avg
+FROM match
+WHERE 
+	hometeam_id = 9908 
+	AND season = '2011/2012';
