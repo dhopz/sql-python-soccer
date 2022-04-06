a567440586c6a5815d4b21b2b01afd3c28aa65be
dhopz <david_houpapa@hotmail.com>
2022-04-06 16:01:13 +0100

    list of teams that scored 8 or more goals in a home match

--- sql_help.sql
+++ sql_help.sql
@@ -24,3 +24,15 @@ LEFT JOIN match AS m
 ON c.id = m.country_id
 -- Group by country name alias
 GROUP BY country;
+
+--list of teams that scored 8 or more goals in a home match.
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT hometeam_id 
+       FROM match
+       WHERE home_goal >= 8);
